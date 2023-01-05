15a65bad9fc153054862d932b4033bef0eafa59d
David <david.katalinic9@gmail.com>
2023-01-06 00:02:32 +0100

    MongoDb conncet

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,12 @@
 const express = require('express');
+const colors = require('colors');
 const dotenv = require('dotenv').config();
 const {errorHandler} = require('./middleware/errorMiddleware');
+const connectDb = require('./config/db');
 
+connectDb();
+
+const CONNECTION_URL = process.env.CONNECTION_URL;
 const port = process.env.PORT || 5000; 
 
 const app = express();
